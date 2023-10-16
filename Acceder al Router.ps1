e28f846c359db817568155edf465b0d316f7d94d
オタコン悪 <otaconeviloficial@gmail.com>
2023-10-16 09:48:02 -0300

    Update Acceder al Router.ps1

--- Acceder al Router.ps1
+++ Acceder al Router.ps1
@@ -1,2 +1,3 @@
+# obtiene la dirección IP de la puerta de enlace predeterminada y abre el navegador web con esa dirección.
 $gatewayIP = (Get-NetRoute | Where-Object { $_.DestinationPrefix -eq '0.0.0.0/0' }).NextHop
 Start-Process "http://$gatewayIP"
