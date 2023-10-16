dc14acb5b80b18f1c2690951e89ce56e4d08b7cf
オタコン悪 <otaconeviloficial@gmail.com>
2023-10-16 09:49:13 -0300

    Update Obtener Puerta de Enlace .ps1

--- Obtener Puerta de Enlace .ps1
+++ Obtener Puerta de Enlace .ps1
@@ -1,4 +1,10 @@
+# obtiene la dirección IP de la puerta de enlace predeterminada,
+# la muestra en la pantalla y espera hasta que el usuario presione Enter para salir.
 $gatewayIP = (Get-NetRoute | Where-Object { $_.DestinationPrefix -eq '0.0.0.0/0' }).NextHop
+
+# Muestra la dirección IP de la puerta de enlace en la pantalla
 $gatewayIP
+
+# Espera a que el usuario presione Enter antes de salir
 Read-Host -Prompt "Presiona Enter para salir..."
 
