7ceb734905a98e01133830e01254114a9ed02ff7
オタコン悪 <otaconeviloficial@gmail.com>
2023-10-16 08:52:19 -0300

    Update Cambiar DNS a los servidores de Googl.bat

--- Cambiar DNS a los servidores de Googl.bat
+++ Cambiar DNS a los servidores de Googl.bat
@@ -1,9 +1,9 @@
 @echo off
-title Cambiar DNS a los servidores de Google
+title Cambiar DNS a los servidores de Google 
 
-echo Cambiando la configuración de DNS a los servidores de Google...
-netsh interface ipv4 set dns "Conexión de red" static 8.8.8.8
-netsh interface ipv4 add dns "Conexión de red" 8.8.4.4 index=2
-echo La configuración de DNS se ha cambiado a los servidores de Google.
+echo Cambiando la configuración de DNS a los servidores de Google ...
+netsh interface ipv4 set dns "Ethernet" static 8.8.8.8
+netsh interface ipv4 add dns "Ethernet" 8.8.4.4 index=2
+echo La configuración de DNS  se ha cambiado a los servidores de Google.
 
 pause
